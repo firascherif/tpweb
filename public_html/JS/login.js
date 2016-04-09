4463528262ddab83b338870216b623ccf7332dfb
firascherif <firas-cherif@hotmail.com>
2016-04-09 15:36:04 -0400

    login page connexion MARCHE

--- public_html/JS/login.js
+++ public_html/JS/login.js
@@ -1,12 +1,9 @@
-
-function validateForm() {
-    var un = document.getElementById("usr").value;
-    var pw = document.getElementById("pword").value;
-    var username = "admin";
-    var password = "admin";
-    if ((un === username) && (pw === password)) {
-        window.location = "infoStade.html";
-    } else {
-        alert("Login was unsuccessful, please check your username and password");
-    }
-}
+ function validateForm() {
+                var un = document.getElementById("usr").value;
+                var pw = document.getElementById("pword").value;
+                if (un == "admin" && pw == "INF2005")
+                    return true;
+                else                   
+                    alert("Login echoue, verifier votre login et mot de passe");
+                    return false;
+            }
