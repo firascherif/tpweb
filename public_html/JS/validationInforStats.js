c27344c6fcbad1ee6fccc8d644be012952019ec1
carolaneFd <fecteau-dorval.carolane@courrier.uqam.ca>
2016-04-17 15:22:45 -0400

    validationsInfoStats commencement et creation dossier plugins

--- public_html/JS/validationInforStats.js
+++ public_html/JS/validationInforStats.js
@@ -1,7 +1,4 @@
-/*
- * 
- * work in progress stades
- * $( document ).ready(function(){
+$( document ).ready(function(){
     $("#myForm").submit(function(){
         var name = $("#Name").val();
         var capacite = $("#capacite").val();
@@ -11,23 +8,41 @@
         var adresse = $("#adresse").val();
         var info = $("#informations").val();
         var date = $("#date").val();
-    }
+     
+    
     var stades = [name,capacite,acces,hauteur,largeur,adresse,info,date];
     var stadesS = JSON.parse(localStorage.getItem("Stades"));
     if(stadesS == null){
         stadesS = [];
+        
     }
     if(stadesS != []){
-        for(var int i = 0; i < stadesS.length; i ++){
-            if(stadesS[i][0] == stade[0]){
+        for(var i = 0; i < stadesS.length; i ++){
+            if(stadesS[i][0] == stades[0]){
+                 stadesS[i][1] = capacite;
+                 stadesS[i][2] = acces;
+                 stadesS[i][3] = hauteur;
+                 stadesS[i][4] = largeur;
+                 stadesS[i][5] = adresse;
+                 stadesS[i][6] = info;
+                 
+            }
+
                 
             }
-            
+           
         }
-    }
-    */
+        stadesS.push(stade);
+    localStorage.setItem("stades",stadesS);
+       
+    })
+    
+    })
     
    
+    
+
+
     
 
 
