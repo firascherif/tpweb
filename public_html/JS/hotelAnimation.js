f4ef7a9a7bedba69b214de044bb1e9f408208e92
firascherif <firas-cherif@hotmail.com>
2016-04-17 02:05:05 -0400

    rectifier animation de l'image dans hotel

--- public_html/JS/hotelAnimation.js
+++ public_html/JS/hotelAnimation.js
@@ -1,10 +1,14 @@
-
 function animateImage() {
-    console.log("Called");
-    $('#image').css({right: '25%'});
-    $('#image').animate({right: '-10%'}, 5000, 'linear', function () {
-        animateImage();
-    });
+    Right();
+}
+function Right() {
+    $("#image").animate({left: "+=250"}, 2500, "swing", Left);
+}
+function Left() {
+    $("#image").animate({left: "-=500"}, 5000, "swing", Revenir);
+}
+function Revenir() {
+    $("#image").animate({left: "+=250"}, 2500, "swing", stop());
 }
 
 $(document).ready(function () {
